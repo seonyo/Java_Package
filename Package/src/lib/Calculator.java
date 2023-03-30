bb3de51012df5ae15a86f5cfe9445eb76ff8f1ed
seonyo <jsy6535@naver.com>
2023-03-30 14:02:38 +0900

    Initial Commit

---
+++ Package/src/lib/Calculator.java
@@ -0,0 +1,9 @@
+package lib;
+
+abstract public class Calculator {
+	public abstract int add(int a, int b);
+		public abstract int subtract(int a, int b);
+		public abstract double average(int []a);
+
+
+}
