072e227443e563c6bc914227f65ce0632450682d
seonyo <jsy6535@naver.com>
2023-04-04 09:17:23 +0900

    Equals class

---
+++ Package/src/pack02/EqualsEx.java
@@ -0,0 +1,17 @@
+package pack02;
+
+public class EqualsEx {
+
+	public static void main(String[] args) {
+		Point a = new Point (2,3); //a객체 생성
+		Point b = new Point (2,3); //b객체 생성
+		Point c = new Point (4,3); //c객체 생성
+		
+		if(a==b) //a객체, b객체, c객체는 각각의 다른 객체의 주소값을 가지고 있음
+			System.out.println("a==b");  //false출력 a와 b는 각각 다른 주소 
+		if(a.equals(b)) // a클래스의 내용과 b클래스의 내용을 비교
+			System.out.println("a is equal to b");
+		if(a.equals(c)) //a
+			System.out.println("a is equal to c");
+	}
+}
