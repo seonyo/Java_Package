472573f781ecde2b21b6b6d2e09a4dd62317a5c9
seonyo <jsy6535@naver.com>
2023-04-04 09:00:20 +0900

    Object class 주석 추가

--- Package/src/pack01/ObjectPropertyEx.java
+++ Package/src/pack01/ObjectPropertyEx.java
@@ -1,7 +1,8 @@
 package pack01;
 
 public class ObjectPropertyEx {
-	public static void print(Object obj) {
+	public static void print(Object obj) { //Object obj = p ---> 형변환
+		//Object클래스는 모든 클래스의 아버지 클래스이다.
 		System.out.println(obj.getClass().getName()); //클래스 이름 출력
 		System.out.println(obj.hashCode()); // 해시코드는 클래스의 코드값
 		System.out.println(obj.toString()); //obj객체를 문자열로 출력
